148928909352926ce4fad90af434c54314907e49
santiago <santiago.federiconi@gmail.com>
2020-03-05 23:01:05 +0000

    takeuntil

---
+++ takeUntil.js
@@ -0,0 +1,24 @@
+const takeUntil = function (arr,condition){
+  let newArray = []
+  for (i = 0 ; i < arr.length ; i ++){
+    if(condition(arr[i])) {
+      break;
+    }
+    else {
+      newArray.push(arr[i]);    
+  }
+  }
+  return newArray;
+}
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
